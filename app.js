eeaa77d523d4c359e08c15fe2a0008c4a0414c7f
Wahaj <wahajakm1234@gmail.com>
2020-08-22 02:08:34 +0500

    changes to app.js

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-mkdir nodejs
-cd nodejs
-aws s3 cp s3://wahaj-webserver/app.js /home/ec2-user/nodejs
-npm install express
+// mkdir nodejs
+// cd nodejs
+// aws s3 cp s3://wahaj-webserver/app.js /home/ec2-user/nodejs
+// npm install express
 
-s3://wahaj-webserver/app.js
+// s3://wahaj-webserver/app.js
